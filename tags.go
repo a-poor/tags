627c82592ca65c5c675b75524183d5a627c20cf2
a-poor <45295232+a-poor@users.noreply.github.com>
2021-10-01 15:22:21 -0700

    Adding starter test code

--- tags.go
+++ tags.go
@@ -1,4 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"reflect"
+)
+
+type User struct {
+	ID          int     `csv:"user_id"`
+	FirstName   string  `csv:"first_name"`
+	LastName    string  `csv:"last_name,omitempty"`
+	CoolnessPct float32 `csv:"coolness_pct"`
+	IsCool      bool    `csv:"is_cool"`
+}
+
 func main() {
+	u := User{1, "Tim", "Tomson", 0.9, true}
+	fmt.Printf("%+v\n", u)
+
+	ut := reflect.TypeOf(u)
+	fmt.Printf("%+v\n", ut)
+	for i := 0; i < ut.NumField(); i++ {
+		field := ut.Field(i)
+		fmt.Printf("[%d] %15q => %q\n", i, field.Name, field.Tag)
+		csv, ok := field.Tag.Lookup("csv")
+		fmt.Printf("csv set: %t   csv value: %q\n", ok, csv)
+		fmt.Println()
+	}
 }
